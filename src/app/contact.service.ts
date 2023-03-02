943857f097b217e275b2ef7da6062728f1bfad13
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2023-03-02 13:27:44 +0300

    add get functions and consume the api

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { Contact } from './contact';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
-
-  constructor(private http: HttpClient) { }
+ 
+  constructor(private http: HttpClient) { 
+    
+  }
+  get() {
+    return this.http.get<Contact[]>('http://localhost:3000/user');
+  }
 }
